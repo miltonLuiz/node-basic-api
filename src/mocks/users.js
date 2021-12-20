f04efa4f6dad3f45fe67e716052e454fb8584b70
Milton Luiz Alvarenga Junior <milton.luiz@infracommerce.com.br>
2021-12-19 22:09:55 -0300

    Just a basic project to heat my Node.JS

---
+++ src/mocks/users.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Milton'
+    },
+    {
+        id: 2,
+        name: 'Luis'
+    },
+    {
+        id: 3,
+        name: 'Barbara'
+    },
+    {
+        id: 4,
+        name: 'Sabrina'
+    },
+    {
+        id: 5,
+        name: 'Julio'
+    },
+    {
+        id: 6,
+        name: 'Marciano'
+    },
+];
